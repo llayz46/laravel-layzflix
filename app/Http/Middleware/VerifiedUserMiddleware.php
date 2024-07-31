ec2972c7b2b8a016ddd2173334739a94233c1398
llayz46 <layzer@outlook.fr>
2024-07-31 22:05:56 +0200

    fix condition on adding comment

--- app/Http/Middleware/VerifiedUserMiddleware.php
+++ app/Http/Middleware/VerifiedUserMiddleware.php
@@ -21,15 +21,7 @@ class VerifiedUserMiddleware
         return static::class . ':' . $route;
     }
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
-     * @param string|null $redirectToRoute
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|null
-     */
-    public function handle($request, Closure $next, $redirectToRoute = null)
+    public function handle(Request $request, Closure $next, string|null $redirectToRoute = null)
     {
         if (!$request->user() ||
             ($request->user() instanceof MustVerifyEmail &&
